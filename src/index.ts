be872ecf5b2cc2a4d6e163ffcd16011f5e06ef64
zS1L3NT <zechariahtan144@gmail.com>
2022-12-28 03:26:27 +0800

    🐛 forgot to include index.ts

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import YTMusic from "./YTMusic"
+
+export type {
+	AlbumBasic,
+	AlbumDetailed,
+	AlbumFull,
+	ArtistBasic,
+	ArtistDetailed,
+	ArtistFull,
+	PlaylistFull,
+	SearchResult,
+	SongDetailed,
+	SongFull,
+	ThumbnailFull,
+	VideoDetailed,
+	VideoFull
+} from "./schemas"
+
+export default YTMusic
